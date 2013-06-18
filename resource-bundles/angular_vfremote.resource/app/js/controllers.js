3209568f0722b825014f47cac84d4c82128c8f77
Andy Schwartz <andy-git@schegg.org>
2013-06-18 16:42:21 -0700

    work commit
    Example getting there.

--- resource-bundles/angular_vfremote.resource/app/js/controllers.js
+++ resource-bundles/angular_vfremote.resource/app/js/controllers.js
@@ -5,10 +5,21 @@
 var myModule = angular.module('angvfr.controllers', []);
 
 myModule
-  .controller('MyCtrl1', ['$scope', 'myFactory', function($scope, myFactory) {
-//  	$scope.answer = myFactory($scope.query);
+  .controller('MyCtrl1', ['$scope', 'doubleMe', function($scope, doubleMe) {
 
-  	$scope.askSFDC = function(input){$scope.sfdcResult = myFactory(input);};
+  	$scope.sfdcResult = '_____';
+
+  	doubleMe(10).then(function(result) {$scope.example1 = result;})
+  	doubleMe('ten').then(function(result) {$scope.example2 = result;})
+
+  	$scope.askSFDC = function(input){
+  		doubleMe(input).then(function(result) {$scope.sfdcResult = result;},
+  					 		 function(error)  {$scope.sfdcResult = 'ERROR: ' + error.message;}
+  							 )
+  	};
+ 
+
+ 	
   }])
 
   .controller('MyCtrl2', [function() {
